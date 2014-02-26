b0f1ba335d610613b8bc8bde2b4385c3a6c9985d
xhzengAIB <xhzengAIB@gmail.com>
2014-02-26 13:07:28 +0000

    design XHInstagramCameraManager api

--- Source/XHInstagramCameraManager.h
+++ Source/XHInstagramCameraManager.h
@@ -7,7 +7,95 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+typedef NS_ENUM(NSInteger, XHFlashMode) {
+    XHFlashModeOff  = AVCaptureFlashModeOff,
+    XHFlashModeOn   = AVCaptureFlashModeOn,
+    XHFlashModeAuto = AVCaptureFlashModeAuto,
+    XHFlashModeLight
+};
+
+typedef NS_ENUM(NSInteger, XHTorchMode) {
+    XHTorchModeOff  = AVCaptureTorchModeOff,
+    XHTorchModeOn   = AVCaptureTorchModeOn,
+    XHTorchModeAuto = AVCaptureTorchModeAuto,
+};
+
+typedef NS_ENUM(NSInteger, XHExposureMode) {
+    XHExposureModeLocked  = AVCaptureExposureModeLocked,
+    XHExposureModeAutoExpose   = AVCaptureExposureModeAutoExpose,
+    XHExposureModeContinuousAutoExposure = AVCaptureExposureModeContinuousAutoExposure,
+};
+
+typedef NS_ENUM(NSInteger, XHWhiteBalanceMode) {
+    XHWhiteBalanceModeLocked  = AVCaptureWhiteBalanceModeLocked,
+    XHWhiteBalanceModeAutoWhiteBalance   = AVCaptureWhiteBalanceModeAutoWhiteBalance,
+    XHWhiteBalanceModeContinuousAutoWhiteBalance = AVCaptureWhiteBalanceModeContinuousAutoWhiteBalance,
+};
+
+typedef NS_ENUM(NSInteger, XHCameraDevice) {
+    XHCameraDeviceBack = AVCaptureDevicePositionBack,
+    XHCameraDeviceFront = AVCaptureDevicePositionFront
+};
+
+typedef NS_ENUM(NSInteger, XHCameraFocusMode) {
+    XHCameraFocusModeLocked = AVCaptureFocusModeLocked,
+    XHCameraFocusModeAutoFocus = AVCaptureFocusModeAutoFocus,
+    XHCameraFocusModeContinuousAutoFocus = AVCaptureFocusModeContinuousAutoFocus
+};
+
+@protocol XHInstagramCameraManagerDelegate <NSObject>
+
+@optional
+// Photo
+- (void)capturePhotoDidFinish:(UIImage *)photo withMetadata:(NSDictionary *)metadata;
+- (void)capturePhotoFailedWithError:(NSError *)error;
+
+// Video
+- (void)videoRecordDidFinish:(NSURL *)videoRecorderUrl didRecordVideoFrame:(CMTime)frameTime;
+- (void)videoRecordFailedWithError:(NSError *)error;
+
+// Error
+- (void)someOtherError:(NSError *)error;
+- (void)acquiringDeviceLockFailedWithError:(NSError *)error;
+
+// Session
+- (void)captureSessionDidStartRunning;
+- (void)cameraSessionDidStop;
+
+@end
 
 @interface XHInstagramCameraManager : NSObject
+
+@property (nonatomic, weak) id <XHInstagramCameraManagerDelegate> delegate;
+@property (nonatomic, readonly, strong) AVCaptureSession *captureSession;
+@property (nonatomic, readonly, strong) AVCaptureDeviceInput *videoInput;
+@property (nonatomic, assign) XHFlashMode flashMode;
+@property (nonatomic, assign) XHTorchMode torchMode;
+@property (nonatomic, assign) XHCameraFocusMode focusMode;
+@property (nonatomic, assign) XHExposureMode exposureMode;
+@property (nonatomic, assign) XHWhiteBalanceMode whiteBalanceMode;
+@property (nonatomic, assign, readonly) NSUInteger cameraCount;
+
+- (void)setCameraMaxScale:(CGFloat)maxScale;
+- (CGFloat)cameraMaxScale;
+- (BOOL)cameraToggle;
+- (BOOL)hasMultipleCameras;
+- (BOOL)hasFlash;
+- (BOOL)hasTorch;
+- (BOOL)hasFocus;
+- (BOOL)hasExposure;
+- (BOOL)hasWhiteBalance;
+
+- (BOOL)setupSessionWithPreset:(NSString *)sessionPreset error:(NSError **)error;
+
+- (void)startRunning;
+- (void)stopRunning;
+- (void)captureImageForDeviceOrientation:(UIDeviceOrientation)deviceOrientation;
+
+- (void)focusAtPoint:(CGPoint)point;
+- (void)exposureAtPoint:(CGPoint)point;
+- (CGPoint)convertToPointOfInterestFrom:(CGRect)frame coordinates:(CGPoint)viewCoordinates layer:(AVCaptureVideoPreviewLayer *)layer;
 
 @end
