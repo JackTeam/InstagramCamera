d971184f35790afaa249253dbe681495cc047400
xhzengAIB <xhzengAIB@gmail.com>
2014-02-26 12:54:31 +0000

    Add XHInstagramCameraManager class

---
+++ Source/XHInstagramCameraManager.m
@@ -0,0 +1,13 @@
+//
+//  XHInstagramCameraManager.m
+//  InstagramCamera
+//
+//  Created by 曾 宪华 on 14-2-26.
+//  Copyright (c) 2014年 HUAJIE QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import "XHInstagramCameraManager.h"
+
+@implementation XHInstagramCameraManager
+
+@end
